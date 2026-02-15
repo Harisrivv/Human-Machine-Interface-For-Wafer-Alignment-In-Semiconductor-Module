7690c341e424df1a7f8307b817dbbeec0a2bcc8d
Harisrivv <146474215+Harisrivv@users.noreply.github.com>
2026-02-15 22:00:17 +0530

    Update config.py

--- firmware/config.py
+++ firmware/config.py
@@ -1,1 +1,45 @@
+# =============================
+# SYSTEM CONFIGURATION FILE
+# =============================
+
+# GPIO MODE
+GPIO_MODE = "BOARD"
+
+# LCD GPIO Pins
+LCD_RS = 7
+LCD_E  = 11
+LCD_D4 = 12
+LCD_D5 = 13
+LCD_D6 = 15
+LCD_D7 = 16
+
+# Motor Pins
+MOTOR_IN1 = 18
+MOTOR_IN2 = 22
+
+# Buttons
+WAFER_BTN = 31
+COUNT_BTN = 32
+RESET_BTN = 33
+STOP_BTN  = 37
+
+# LEDs
+RED_LED   = 35
+GREEN_LED = 38
+
+# ADC Channel
+LDR_CHANNEL = 0
+
+# LCD Constants
+LCD_WIDTH = 16
+LCD_LINE_1 = 0x80
+LCD_LINE_2 = 0xC0
+E_PULSE = 0.0005
+E_DELAY = 0.0005
+
+# Alignment Threshold
+ALIGN_THRESHOLD = 40
+
+# Maximum Wafer Count
+MAX_WAFER_COUNT = 12
 
