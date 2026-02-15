19c66a4975adc96aa3c37ae1064b213ee84c6f54
Harisrivv <146474215+Harisrivv@users.noreply.github.com>
2026-02-15 22:00:44 +0530

    Update adc_driver.py

--- firmware/adc_driver.py
+++ firmware/adc_driver.py
@@ -1,1 +1,16 @@
+import spidev
+
+spi = spidev.SpiDev()
+
+def init_adc():
+    spi.open(0, 0)
+    spi.max_speed_hz = 1000000
+
+def read_adc(channel):
+    adc = spi.xfer2([1, (8 + channel) << 4, 0])
+    data = ((adc[1] & 3) << 8) + adc[2]
+    return data
+
+def close_adc():
+    spi.close()
 
